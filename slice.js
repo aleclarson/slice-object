6252a7c879caebd56d7a97f503b2cefcdb88c104
aleclarson <alec.stanford.larson@gmail.com>
2018-07-16 17:51:52 -0400

    perf: minor improvement to left-padded slices
    This avoids object access when we know the property will be undefined,
    because the object is assumed to be zero-based.

--- slice.js
+++ slice.js
@@ -1,7 +1,10 @@
 module.exports =
 function slice(obj, start) {
-  var arr = [], len = obj.length;
-  var i = (start < 0 ? start += len : start) || 0;
+  var arr = [], len = obj.length, i = start || 0;
+  if (start < 0) {
+    i = start += len;
+    while (i < 0) arr[i++ - start] = undefined;
+  }
   while (i < len) arr[i - start] = obj[i++];
   return arr;
 }
