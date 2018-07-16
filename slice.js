ea7cc8e4c91a33a30cde33f72bb8b185efce9023
aleclarson <alec.stanford.larson@gmail.com>
2018-07-16 17:30:56 -0400

    fix: always start the new array at index zero

--- slice.js
+++ slice.js
@@ -1,6 +1,6 @@
 module.exports =
 function slice(obj, start) {
   var arr = [], i = start || 0, len = obj.length;
-  while (i < len) arr[i] = obj[i++];
+  while (i < len) arr[i - start] = obj[i++];
   return arr;
 }
